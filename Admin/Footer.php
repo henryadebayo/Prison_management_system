a0a2f31177d4a64b497f8eec61b9a0f4ade04bad
Hussaini Maina Usman <hussainmaina27@gmail.com>
2021-07-10 18:35:21 +0100

    updated the system to include footer.php, deletetransfer.php, jquery.js,location.php and login.css

---
+++ Admin/Footer.php
@@ -0,0 +1,4 @@
+<tr>
+<td bgcolor="#06640a" colspan="3" align="center">
+&copy; DEVELOPED BY FCP/CSC/18/AlpaGroup </td>
+</tr>
---
+++ Admin/location.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+  <title>KANO CENTRAL PRISON</title>
+   <link rel="stylesheet" media="screen" href="login.css" >
+</head>
+<body>
+	<table align='center' border='0' bgcolor='green' width='820' cellpadding='10' cellspacing='0' height='325'>
+          
+		  <tr>
+            <td colspan='3' height='2'><img src='banner.gif'></td>
+          </tr>
+		  <tr>
+            <td colspan="7" bgcolor="#yellow" height="1" align="center">
+		      <h1><font size="5">
+	           <a href="index.php">Home</a> | 
+              			   
+		        <a href="search-form.php">Search </a> 
+		        <a href="location.php">Location</a>|
+		        <a href="announce.php">Complain</a>|
+				 <a href="transfer.php">Prisoner Trans </a> ||
+		        <a href="officertransfer.php">Warder</a>|
+		        <a href="registration.php">Register</a>
+				<a href="court.php">Court</a>
+		        </font></h1>
+            </td>
+			 </td>
+		 
+            <td height='1' colspan='3' align='right' bgcolor="#006600">&nbsp;</td>
+			
+          </tr>
+		 
+          <tr>
+		 
+            <td width='4%' bgcolor='#FFFFFF' valign='top'>
+<h3 align='center'  title='You should be online'>&nbsp;</h3></td>
+
+            <td width='11%' valign='top' bgcolor="#FFFFFF">
+
+<p align='center'>
+ 
+
+
+<br/>
+
+			<div class="clearfix"></div>
+        </div>
+    </div>
+<br>
+</td>
+ <td width='75%' bgcolor='green'  valign='top'>	
+<table border='0' align='center'>
+<tr>
+<td width="552" bgcolor="green">
+<h3>  Admin Management : </h3><br/>
+<ul>
+<li><a href='viewofficer.php'><b>View Warder Transfer</b></a></li>
+		<br>
+	<li><a href='viewprisoners.php'><b>Inmates Information Display</b></a></li>
+		<br>
+		<li><a href='viewcase.php'><b>Case Information View</b></a></li>
+		<br>
+	<li><a href='viewtransfer.php'><b>Inmates Transfer Information</b></a></li>
+		<br>
+	<li><a href='viewAnnounce.php'><b>View Comment</b></a></li>
+		<br>
+    <li><a href='../index.php''><b>LOG OUT</b></a></li>
+</ul>
+</td>
+</tr>
+</table>
+
+
+			
+			</td>
+          </tr>
+          <tr>
+            <td colspan='3' align='center' bgcolor='#FF0000' height='1'>
+					&copy; <strong>
+                &nbsp;DEVELOPED BY FCP/CSC/18/ALPHAGROUP </strong></td>
+          </tr>
+	</table>
+</body>
+</head>
+</html>
